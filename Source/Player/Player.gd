35729a6c94f736b10bfdfcd40353d087a32e1cef
Dzuniga <dave.zuna@gmail.com>
2020-03-07 16:09:24 -0500

    animated player

--- Source/Player/Player.gd
+++ Source/Player/Player.gd
@@ -9,8 +9,10 @@ export(float) var acceleration = 50.0
 export(float) var gravity = 10.0
 
 var jumping = false
+var isLookingLeft = false
 
 var keys = []
+onready var anim = get_node("AnimatedSprite")
 
 
 func _ready():
@@ -18,16 +20,27 @@ func _ready():
 
 
 func _physics_process(delta):
+	var on_floor = is_on_floor()
 	motion.y += gravity
 	
 	if Input.is_action_pressed("move_right"):
 		motion.x = min(motion.x + acceleration, max_speed)
+		if(isLookingLeft):
+			isLookingLeft = !isLookingLeft
+			scale.x *= -1
+		if(on_floor):
+			anim.play("MoveRight")
 	elif Input.is_action_pressed("move_left"):
 		motion.x = max(motion.x - acceleration, -max_speed)
+		if(!isLookingLeft):
+			isLookingLeft = !isLookingLeft
+			scale.x *= -1
+		if(on_floor):
+			anim.play("MoveRight")
 	else:
 		motion.x = 0
+		anim.play("Idle")
 	
-	var on_floor = is_on_floor()
 	
 	if on_floor and Input.is_action_just_pressed("jump"):
 		motion.y = -jump_power
