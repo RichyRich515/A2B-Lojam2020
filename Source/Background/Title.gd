23ec67fc2cd52b1e6a6c693bb1cccdca11ab0b85
Richard Woods <rm-woods@hotmail.com>
2020-03-08 10:39:40 -0400

    Added particle effect, fixed credits, Level 5 & 6

--- Source/Background/Title.gd
+++ Source/Background/Title.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -40)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -20)
 
 func _input(event):
 	if event is InputEventKey:
